a37c2ab887c506319ad0219379d8d8958315580b
Omnia <99294745+omnia-5@users.noreply.github.com>
2024-04-12 16:51:21 +0200

    Add files via upload

---
+++ Members/Omnia Ahmed/Task 2/src/TakeData.js
@@ -0,0 +1,46 @@
+// Deal as child component
+
+import styles from './styles.module.css';
+import PropTypes from "prop-types";
+import React from 'react';
+const TakeData = ({ movieDetails }) => {
+    return (
+        <div className={styles.container}>
+            {movieDetails.slice(0, 3).map((data) => (
+                <div key={data.id} className={styles.box}>
+                    {data.title ? (
+                        <h3 className={styles.movie_title}>{data.title}</h3>) :
+                        (<h3>Title is Required</h3>)}
+                    <img className={styles.images} src={data.posterPath} alt={data.title} />
+                    <p className={styles.overview}>overview :</p>
+                    {data.overview ? (<p className={styles.description}>{data.overview}</p>) :
+                        (<p className={styles.description}>There is not a description it is mine</p>)}
+                </div>
+            ))}
+            {movieDetails.slice(3).map((data) => (
+                <div key={data.id} className={styles.box} id={styles.secondRow}>
+                    {data.title ? (
+                        <h3 className={styles.movie_title}>{data.title}</h3>) :
+                        (<h3>Title is Required</h3>)}
+                    <img className={styles.images} src={data.posterPath} alt={data.title} />
+                    <p className={styles.overview}>overview :</p>
+                    {data.overview ? (<p className={styles.description}>{data.overview}</p>) :
+                        (<p className={styles.description}>There is not a description it is mine</p>)}
+                    
+                </div>
+            ))}
+            </div>
+    );
+};
+
+TakeData.propTypes = {
+    movieDetails: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+
+export default TakeData;
